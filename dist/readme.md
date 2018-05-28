158dfcd47ed58e695b30be7ddee43f0840d6f810
Jay Mistry <mistry.jay15@gmail.com>
2018-05-28 15:08:57 +0530

    Initial Commit

---
+++ dist/readme.md
@@ -0,0 +1,8 @@
+#Youtube Downloader App
+
+##How to use:
+Follow these steps to dowload a playlist:
+-Copy playlist url
+-Start ytdownloader.exe and paste the url
+-Once the download.bat file is generated, move the download.bat and youtube-dl.exe files in your destination folder.
+-Start the download.bat file and your playlist should be downloaded.
---
+++ ytdownloader.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def main():
+	print("Welcome to Youtube Playlist Downloader v1.0.0 \nThis is a better way to download complete playlists from YouTube.\n")
+	url = str(input('Enter the playlist URL : '))
+	print("Collecting page data...")
+	try:
+		source_code = requests.get(url)
+		print("Page Downloaded.")
+		print("Extracting Links...")
+		plain_text = source_code.text
+		soup = BeautifulSoup(plain_text, 'html.parser')
+		megastr = ""
+		for link in soup.find_all('a'):
+			x = str(link.get('href'))
+			if "watch" in x:
+				megastr += 'youtube-dl https://www.youtube.com/' + x + "\n"
+		megastr += "pause"
+		print("Generating batch file...")
+		fw = open('download.bat', 'w')
+		fw.write(megastr)
+		fw.close
+		#break
+	except requests.exceptions.MissingSchema:
+		print("Insert the URL along with 'https://' or 'http://' and try again.")
+		os.system("pause")
+		os.system("cls")
+		main()
+	except requests.exceptions.ConnectionError:
+		print("Could not retrieve data from the Internet. Please try again after a connection is established.")
+		os.system("pause")
+		os.system("cls")
+		main()
+	finally:
+		print("File created successfully.\n\nInstructions:\nMove the 'youtube-dl.exe' and 'download.bat' file in your destination folder, \nDouble-click the batch file to start download.\n\nThank you for using this software :-)\nMade by Jay Mistry.")
+		os.system("pause")
+
+main()
